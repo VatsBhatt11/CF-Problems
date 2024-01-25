0478f74f88f45297178ab575142ad32d8ba2d461
VatsBhatt11 <vatsalmbhatt11@gmail.com>
2024-01-25 14:23:04 +0530

    Solved k_Factorization problem.

---
+++ A_k_Factorization.cpp
@@ -0,0 +1,129 @@
+#include "bits/stdc++.h"
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+void helper(int n, unordered_map<int, int> &m, int &s)
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
+            int cnt = 0;
+            while (n % i == 0)
+            {
+                cnt++;
+                n /= i;
+            }
+            m[i] = cnt;
+            s += cnt;
+        }
+    }
+    if (n > 1)
+    {
+        m[n] = 1;
+        s += 1;
+    }
+}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    unordered_map<int, int> m;
+    int s = 0;
+    helper(n, m, s);
+    if (k > s)
+        cout << -1;
+    else
+    {
+        int p = 1;
+        for (auto &it : m)
+        {
+            int a = it.ff, b = it.ss;
+            while(k>1 && b)
+            {
+                cout << a << " ";
+                k--;
+                b--;
+            }
+            if(k==1)
+                p*=pow(a,b);
+        }
+        if(k==1)
+            cout << p;
+    }
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
